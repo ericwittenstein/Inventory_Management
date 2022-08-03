fcf7eab2cccfea772f3fa60c21d7432a0067de8f
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-03 11:36:34 -0400

    fixed server.js file and controllers/index.js file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // initial connection imports
 const express = require("express");
+const exphbs = require('express-handlebars');
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
@@ -8,14 +9,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // handlebars
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 // express connections
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+
 app.use(routes);
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log("Now listening"));
