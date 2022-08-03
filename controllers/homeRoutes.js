1a9e502d37144e960b519c0b66a6e09f7f07b89f
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-03 18:03:42 -0400

    created the render routes for the employee and item tables, as well as the handlebars landing pages for each

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,30 @@
 // handle all rendering routes
+const router = require("express").Router();
+
+// route to render the table of employees from the db
+router.get("/employees", async (req, res) => {
+	db.query("QUERY TEXT GOES HERE", (error, res, fields) => {
+		if (error) {
+			res.status(500);
+			res.send(error);
+		} else {
+			res.status(200);
+			res.render("employeeLanding", { data: res });
+		}
+	});
+});
+
+// route to render the table of items from the db
+router.get("/items", async (req, res) => {
+	db.query("QUERY TEXT GOES HERE", (error, res, fields) => {
+		if (error) {
+			res.status(500);
+			res.send(error);
+		} else {
+			res.status(200);
+			res.render("itemLanding", { data: res });
+		}
+	});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const path = require("path");
 
 // express
 const app = express();
@@ -16,7 +17,7 @@ app.set("view engine", "handlebars");
 // express connections
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 sequelize.sync({ force: false }).then(() => {
