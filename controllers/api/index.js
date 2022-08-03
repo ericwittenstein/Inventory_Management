278d9aa1b7cdce94024918766fd591a080307ee7
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-03 19:16:41 -0400

    made post routes to create new item and new employee

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
+const employeeRoutes = require("./employeesRoutes");
+const itemRoutes = require("./itemsRoutes")
+
+router.use('/employees', employeeRoutes);
+router.use('/items', itemRoutes);
 
 module.exports = router;
