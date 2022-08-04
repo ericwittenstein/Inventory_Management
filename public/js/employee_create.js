db9dd7630c5f9e3ca3f8f48d1c1e3630e337059b
KimboSlice <kimberlyhoward529@gmail.com>
2022-08-04 12:09:30 -0400

    updated function to create employee

--- public/js/employee_create.js
+++ public/js/employee_create.js
@@ -1,26 +1,38 @@
-const { response } = require("express");
-const addEmpBtn = document.getElementById('add-employee-btn')
+// const { response } = require("express");
+const addEmpBtn = document.querySelector('#add-employee-btn');
 
 const addEmployeeBtn = async (e) => {
     // Prevent default form setting
     e.preventDefault();
 
     // Bring in input data
-    const firstName = documet.querySelector('#first-name').value.trim();
-    const lastName = documet.querySelector('#last-name').value.trim();
+    const firstName = document.querySelector('#first-name').value;
+    console.log(firstName);
+    const lastName = document.querySelector('#last-name').value.trim();
+    console.log(lastName);
 
     // If user inputs data
     if(firstName && lastName) {
-        const response = await fetch('/api/employees', {
+        const response = await fetch('/employees', {
             method: 'POST',
             body: JSON.stringify({ firstName, lastName }),
             headers: { 'Content-Type': 'application/json'}
         });
+
+        if(response.ok) {
+            document.location.replace('/employees');
+        }
+    } else {
+        console.log("first name and last name not entered")
     }
 
-    if(response.ok) {
-        document.location.replace('/employees');
-    }
+    // if(response.ok) {
+    //     document.location.replace('/employees');
+    // }
 }
 
-addEmpBtn.addEventListener('click', addEmployeeBtn);
+if(addEmpBtn) {
+    addEmpBtn.addEventListener('click', addEmployeeBtn);
+} else {
+    console.log("button does not exist")
+}
