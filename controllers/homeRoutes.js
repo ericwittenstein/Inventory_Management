8655bb4cb48ec332c9ce1a6a3c825b7be4ea3e45
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-04 00:35:09 -0400

    get routes in homeroutes.js now working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,13 @@
 // handle all rendering routes
 const router = require("express").Router();
+const { Employee, Item } = require("../models");
 
 // route to render the table of employees from the db
 router.get("/employees", async (req, res) => {
 	try {
-		db.query("QUERY TEXT GOES HERE", (res, fields) => {
-			res.status(200).render("employeeLanding", { data: res });
-		});
+		const emplData = await Employee.findAll();
+		const newEmplData = res.json(emplData);
+		res.status(200).render("employeeLanding", { data: newEmplData });
 	} catch (error) {
 		res.status(500).json(error);
 	}
@@ -15,9 +16,9 @@ router.get("/employees", async (req, res) => {
 // route to render the table of items from the db
 router.get("/items", async (req, res) => {
 	try {
-		db.query("QUERY TEXT GOES HERE", (res, fields) => {
-			res.status(200).render("itemLanding", { data: res });
-		});
+		const itemData = await Item.findAll();
+		const newItemData = res.json(itemData);
+		res.status(200).render("itemLanding", { data: newItemData });
 	} catch (error) {
 		res.status(500).json(error);
 	}
